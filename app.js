59d45e06dbb0048578e32769de980def3753fd14
emariebetson <52430633+emariebetson@users.noreply.github.com>
2019-10-11 19:00:59 -0500

    Harry Potter Update

--- app.js
+++ app.js
@@ -1,33 +1,308 @@
-/* global variables */
-var questionDiv = document.getElementById('question');
-var choicesDiv = document.getElementById('choices');
-var questionBtn = document.getElementById('questionBtn');
-var startQuestion = 0;
+var questions = [{
+    question: "1) According to the Dursleys, how did Harry’s parents die?",
+    choices: ["msg('Plane Crash')", "msgBox('Train Accident');", "alertBox('Bus Crash');", "alert('Car Accident');"],
+    correctAnswer: 3
+}, {
+    question: "2. What’s the LAST line of the book, Harry Potter and the Sorcerer’s Stone?",
+    choices: ["msg('Goodbye, for now, Hogwarts.')", "msgBox('Harry, Ron, and Hermione looked at each other sheepishly and smiled.');", "alertBox('I'm going to have a lot of fun with Dudley this summer...');", "alertBox('said Dumbledore with a nod.');"],
+    correctAnswer: 2
+}, {
+    question: "3. What is Dumbledore’s full name?",
+    choices: ["msg('Albus Wulfric Percival Brian Dumbledore')", "msgBox('Albus Percival Wulfric Brian Dumbledore');", "alertBox('Albus Percival Brian Wulfric Dumbledore');", "alertBox('Albus Brian Percival Wulfric Dumbledore');"],
+    correctAnswer: 1
+}, {
+    question: "1. What is Lord Voldemort’s real name?",
+    choices: ["msg('Tom Marvolo Riddle')", "msgBox('Tom Marvilo Riddle');", "alertBox('Tom Marvin Riddle');", "alert('Tom Ravalo Riddle');"],
+    correctAnswer: 0
+}, {
+    question: "5. What does S.P.E.W. stand for?",
+    choices: ["msg('Society For the Promotion of Elfish Welfare')", "msgBox('Society For the Protection of Elves and Wizards');", "alertBox('Support For People, Elves, and Wizards');", "alert('Sorority For the Protection of Elves and Wands');"],
+    correctAnswer: 0
+},{
+	question: "6. Who gave us the Marauder’s Map?",
+    choices: ["Messrs Moody, Wormtongue, Sappen, and Frongs", "Messrs Moony, Wormtail, Padfoot, and Prongs", "Messrs Molly, Wilfred, Peter, and Phongs", "Messrs Matchstick, Winston, Footfed, and Trongs"],
+    correctAnswer: 1
+},{
+	question: "7. A person born into a wizarding family who cannot do magic is called…",
+    choices: ["A Muggle", "A Half-Blood", "A Mudblood", "A Squib"],
+    correctAnswer: 3
+},{
+	question: "8. What is the symbol for the Ravenclaw house?",
+    choices: ["Raven", "Crow", "Eagle", "Hawk"],
+    correctAnswer: 2
+},{
+	question: "9. How many Sickles are in a Galleon?",
+    choices: ["13", "17", "15", "19"],
+    correctAnswer: 1
+},{
+	question: "10. What are the three cores Garrick Ollivander uses in his wand-making?",
+    choices: ["Phoenix feathers, dragon heartstring, and unicorn hair", "Phoenix feathers, dragon tooth, and centaur mane", "Dragon heartstring, unicorn horn, and centaur mane", "Phoenix beak, dragon heartstring, unicorn hair"],
+    correctAnswer: 0
+}];
 
-/* helpers */
-/* takes in index, uses that index to find question in db to return the title */
-function renderQuestion(index) {
-    return questions[index].title;
-};
-/* takes in index, uses this index to return the choices from the db */
-function renderChoices(index) {
-    choicesDiv.innerHTML = '';
-    for(i = 0; i < questions[index].choices.length; i++) {
-        var choiceBtn = document.createElement('button');
-        choiceBtn.textContent = questions[index].choices[i];
-        choicesDiv.appendChild(choiceBtn);
-    };
-}
 
+var currentQuestion = 0;
+var viewingAns = 0;
+var correctAnswers = 0;
+var quizOver = false;
+var iSelectedAnswer = [];
+	var c=180;
+	var t;
+$(document).ready(function () 
+{
+    // Display the first question
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+    $(this).find(".preButton").attr('disabled', 'disabled');
+	
+	timedCount();
+	
+	$(this).find(".preButton").on("click", function () 
+	{		
+		
+        if (!quizOver) 
+		{
+			if(currentQuestion == 0) { return false; }
+	
+			if(currentQuestion == 1) {
+			  $(".preButton").attr('disabled', 'disabled');
+			}
+			
+				currentQuestion--; // Since we have already displayed the first question on DOM ready
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 					
+		} else {
+			if(viewingAns == 3) { return false; }
+			currentQuestion = 0; viewingAns = 3;
+			viewResults();		
+		}
+    });
 
-/* events */
-questionBtn.addEventListener('click', function() {
-    startQuestion++;
-    /* call render question */
-    questionDiv.innerHTML = renderQuestion(startQuestion);
-    renderChoices(startQuestion);
+	
+	// On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () 
+	{
+        if (!quizOver) 
+		{
+			
+            var val = $("input[type='radio']:checked").val();
+
+            if (val == undefined) 
+			{
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            } 
+			else 
+			{
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
+				if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				iSelectedAnswer[currentQuestion] = val;
+				
+				currentQuestion++; // Since we have already displayed the first question on DOM ready
+				if(currentQuestion >= 1) {
+					  $('.preButton').prop("disabled", false);
+				}
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 
+				else 
+				{
+					displayScore();
+					$('#iTimeShow').html('Quiz Time Completed!');
+					$('#timer').html("You scored: " + correctAnswers + " out of: " + questions.length);
+					c=185;
+					$(document).find(".preButton").text("View Answer");
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+					
+				}
+			}
+					
+		}	
+		else 
+		{ // quiz is over and clicked the next button (which now displays 'Play Again?'
+			quizOver = false; $('#iTimeShow').html('Time Remaining:'); iSelectedAnswer = [];
+			$(document).find(".nextButton").text("Next Question");
+			$(document).find(".preButton").text("Previous Question");
+			 $(".preButton").attr('disabled', 'disabled');
+			resetQuiz();
+			viewingAns = 1;
+			displayCurrentQuestion();
+			hideScore();
+		}
+    });
 });
 
-/* init */
-questionDiv.innerHTML = renderQuestion(startQuestion);
-renderChoices(0);
+
+
+function timedCount()
+	{
+		if(c == 185) 
+		{ 
+			return false; 
+		}
+		
+		var hours = parseInt( c / 3600 ) % 24;
+		var minutes = parseInt( c / 60 ) % 60;
+		var seconds = c % 60;
+		var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);            
+		$('#timer').html(result);
+		
+		if(c == 0 )
+		{
+					displayScore();
+					$('#iTimeShow').html('Quiz Time Completed!');
+					$('#timer').html("You scored: " + correctAnswers + " out of: " + questions.length);
+					c=185;
+					$(document).find(".preButton").text("View Answer");
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+					
+		}
+		
+		/*if(c == 0 )
+		{	
+			if (!quizOver) 
+			{
+				var val = $("input[type='radio']:checked").val();
+            	if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				currentQuestion++; // Since we have already displayed the first question on DOM ready
+				
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					c=15;
+				} 
+				else 
+				{
+					displayScore();
+					$('#timer').html('');
+					c=16;
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+				}
+			}
+			else 
+			{ // quiz is over and clicked the next button (which now displays 'Play Again?'
+				quizOver = false;
+				$(document).find(".nextButton").text("Next Question");
+				resetQuiz();
+				displayCurrentQuestion();
+				hideScore();
+			}		
+		}	*/
+		c = c - 1;
+		t = setTimeout(function()
+		{
+			timedCount()
+		},1000);
+	}
+	
+	
+// This displays the current question AND the choices
+function displayCurrentQuestion() 
+{
+
+	if(c == 185) { c = 180; timedCount(); }
+    //console.log("In display current Question");
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+    for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			$('<li><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		} else {
+			$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		}
+    }
+}
+
+function resetQuiz()
+{
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore()
+{
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() 
+{
+    $(document).find(".result").hide();
+}
+
+// This displays the current question AND the choices
+function viewResults() 
+{
+
+	if(currentQuestion == 10) { currentQuestion = 0;return false; }
+	if(viewingAns == 1) { return false; }
+
+	hideScore();
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+	for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li style="border:2px solid red;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		} else {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		}
+    }
+	
+	currentQuestion++;
+	
+	setTimeout(function()
+		{
+			viewResults();
+		},3000);
+}
